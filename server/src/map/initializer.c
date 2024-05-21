59394aa9e75cebae123883695d936d02b20450b9
MartinFillon <martin.fillon@hotmail.com>
2024-05-21 09:48:48 +0200

    feat: add food filler and consts for other densitys

--- server/src/map/initializer.c
+++ server/src/map/initializer.c
@@ -6,6 +6,8 @@
 */
 
 #include <stdlib.h>
+
+#include "define.h"
 #include "map.h"
 #include "types/map.h"
 
@@ -28,7 +30,19 @@ map_t *create_map(size_t x, size_t y)
     return map;
 }
 
+static void fill_food(map_t *map, size_t number)
+{
+    int x = 0;
+    int y = 0;
+
+    for (size_t i = 0; i < number; i++) {
+        x = rand() % map->x;
+        y = rand() % map->y;
+        map->arena[y][x].content.food += 1;
+    }
+}
+
 void fill_map(map_t *map)
 {
-    (void)map;
+    fill_food(map, map->x * map->y * FOOD_DENSITY);
 }
