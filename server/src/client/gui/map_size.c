1213227d2f67414b3f145f1a38e2150cef1cd372
MartinFillon <martin.fillon@hotmail.com>
2024-05-22 14:08:40 +0200

    feat: add buffer breaking

---
+++ server/src/client/gui/map_size.c
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy
+** File description:
+** map_size
+*/
+
+#include <stdio.h>
+
+#include "types/client.h"
+
+void map_size(char *args, client_t *cli, game_t *game)
+{
+    if (cli->type != GUI)
+        return;
+    if (args[0] != '\0') {
+        dprintf(cli->fd, "sbp\n");
+        return;
+    }
+    dprintf(cli->fd, "msz %lu %lu\n", game->map.x, game->map.y);
+}
--- server/src/client/gui_entrypoint.c
+++ server/src/client/gui_entrypoint.c
@@ -6,11 +6,23 @@
 */
 
 #include <stdio.h>
+#include <string.h>
+#include "gui/list.h"
 #include "types/client.h"
 
 int gui_entrypoint(char const *line, client_t *c, game_t *game)
 {
-    (void)game;
-    dprintf(c->fd, "Gui received: %s\n", line);
+    char *ln_cpy = strdup(line);
+    char *cmd = strtok(ln_cpy, " ");
+
+    dprintf(2, "CMD: %s\n", cmd);
+    dprintf(2, "ARGS: %s\n", ln_cpy);
+    for (int i = 0; CMDS[i].cmd; i++) {
+        if (strcmp(CMDS[i].cmd, cmd) == 0) {
+            CMDS[i].func(ln_cpy, c, game);
+            return 0;
+        }
+    }
+    dprintf(c->fd, "suc\n");
     return 0;
 }
