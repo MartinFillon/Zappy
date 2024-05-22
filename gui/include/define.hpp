dbcb17c2ef557b4a7041386e2763362f6de1ce97
Manuel Tomé <manuel.tome@epitech.eu>
2024-05-22 17:44:02 +0200

    feat(gui): handle display & server via thread

---
+++ gui/include/define.hpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2024
+** Zappy
+** File description:
+** define
+*/
+
+#pragma once
+
+#define SUCCESS 0
+#define ERROR 84
+
--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -7,26 +7,54 @@
 
 #include <iostream>
 
+#include "define.hpp"
 #include "ArgParser/ArgParser.hpp"
+#include "Display/Display.hpp"
+#include "Network/NetworkHandler.hpp"
 
-int main(int argc, char *argv[])
+static void parseArguments(
+    int argc,
+    char *argv[],
+    int &port,
+    std::string &machine,
+    bool &debug
+)
 {
     ArgParser parser;
-    int port;
-    std::string machine;
-
     parser.setDefault("p", 4242, true)
-        .setDefault("h", std::string("local"), true);
+        .setDefault("h", std::string("localhost"), true)
+        .setDefault("d", false, false);
+
     try {
         parser.parse(argc, argv);
         port = parser.get<int>("p");
         machine = parser.get<std::string>("h");
+        debug = parser.get<bool>("d");
     } catch (const ArgParserException &e) {
         std::cerr << "Error: " << e.what() << " in " << e.where() << std::endl;
-        return 84;
+        throw std::runtime_error("Argument parsing failed");
     }
-    std::cout << "Machine: " << machine << std::endl;
-    std::cout << "Port: " << port << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    int port;
+    std::string machine;
+    bool debug;
+
+    try {
+        parseArguments(argc, argv, port, machine, debug);
+    } catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return ERROR;
+    }
+    NetworkHandler networkHandler(machine, port);
+    networkHandler.start();
+
+    Display display(networkHandler, debug);
+    display.run();
+
+    networkHandler.stop();
 
     return 0;
 }
