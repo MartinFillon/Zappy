e489450815e3c94b18b14269bedd5305faf5dba3
MartinFillon <martin.fillon@epitech.eu>
2024-05-29 20:18:07 +0200

    feat: add base last types

---
+++ ai/src/json/array.rs
@@ -0,0 +1,7 @@
+use super::{JsonValue, Parser, ParserError};
+
+impl<'a> Parser<'a> {
+    pub(super) fn parse_array(&mut self) -> Result<JsonValue, ParserError> {
+        todo!();
+    }
+}
---
+++ ai/src/json/bool.rs
@@ -0,0 +1,7 @@
+use super::{JsonValue, Parser, ParserError};
+
+impl<'a> Parser<'a> {
+    pub(super) fn parse_bool(&mut self) -> Result<JsonValue, ParserError> {
+        todo!();
+    }
+}
