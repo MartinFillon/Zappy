6a9acbb9ad24dd37d16a96a859ff6fa5ac8eae35
rahul.chander <rahul.chander@epitech.eu>
2024-05-30 13:50:39 +0200

    feat(handle_broadcast.c): Sending message to everyone with tile 0

--- server/src/ai/handle_broadcast.c
+++ server/src/ai/handle_broadcast.c
@@ -5,11 +5,26 @@
 ** handle_broadcast
 */
 
+#include <sys/socket.h>
+
 #include "utils.h"
 #include "types/client.h"
 #include "client.h"
 
-// Send to everyone the message and set the distance broadcast sound.
+static inline bool valid_client(client_t *cli1, client_t *cli2)
+{
+    return cli1->fd != 0 && cli1->ai->id != cli2->ai->id;
+}
+
+static void send_to_everyone(char const *msg, client_t *clients, client_t *cli)
+{
+    for (int i = 0; i < SOMAXCONN; i++) {
+        if (valid_client(&clients[i], cli)) {
+            send_client(&clients[i], "message %d, %s\n", 0, msg);
+        }
+    }
+}
+
 void handle_broadcast(
     char const *arg,
     client_t *cli,
@@ -24,4 +39,5 @@ void handle_broadcast(
     if (is_empty(arg))
         return send_client(cli, "ko\n");
     send_client(cli, "ok\n");
+    send_to_everyone(arg, clients, cli);
 }
