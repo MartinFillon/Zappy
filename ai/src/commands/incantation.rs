1856889c84d19dd1abca11d1ead9783a3e15247b
Alexandre VIGOUREUX <alexandre.vigoureux@epitech.eu>
2024-05-30 16:32:16 +0200

    refactor(incantation): using check_dead()

--- ai/src/commands/incantation.rs
+++ ai/src/commands/incantation.rs
@@ -7,44 +7,34 @@
 
 #![allow(dead_code)]
 
+use crate::tcp::command_handle::CommandHandler;
 use crate::tcp::TcpClient;
 
 pub async fn incantation(client: &mut TcpClient) -> Result<Option<usize>, bool> {
-    if client
-        .send_request(String::from("Incantation\n"))
-        .await
-        .is_err()
-    {
-        return Err(true);
-    }
-    match client.get_response().await {
-        Some(res1) => {
-            print!("{res1}");
-            if res1 == "dead\n" {
-                return Err(false);
-            }
-            if res1 == "Elevation underway\n" {
-                match client.get_response().await {
-                    Some(res2) => {
-                        print!("{res2}");
-                        if res2 == "ko\n" {
-                            Ok(None)
-                        } else {
-                            match res2.trim_end().split_once(": ") {
-                                Some((_, nb)) => match nb.parse::<usize>() {
-                                    Ok(k) => Ok(Some(k)),
-                                    Err(_) => Err(true),
-                                },
-                                None => Err(true),
-                            }
-                        }
-                    }
-                    None => Err(true),
+    let res1 = client.check_dead("Incantation\n").await?;
+    match res1.as_str() {
+        "ko\n" => Ok(None),
+        "Elevation underway\n" => {
+            if let Some(res2) = client.get_response().await {
+                match res2.as_str() {
+                    "ko\n" => Ok(None),
+                    "dead\n" => Err(false),
+                    level if res2.starts_with("Current level:") => match level
+                    .trim_end()
+                    .split_once(": ")
+                    .unwrap_or_default()
+                    .1
+                    .parse::<usize>()
+                    {
+                        Ok(k) => Ok(Some(k)),
+                        Err(_) => Err(true),
+                    },
+                    _ => Err(true),
                 }
             } else {
-                Ok(None)
+                Err(true)
             }
         }
-        None => Err(true),
+        _ => Err(true),
     }
 }
