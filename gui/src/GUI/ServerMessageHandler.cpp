37893c474f993debc162e95488d5d2050fb96a81
Manuel Tomé <manuel.tome@epitech.eu>
2024-06-01 04:11:04 +0200

    feat(gui/receivedMsg): egg related cmd

--- gui/src/GUI/ServerMessageHandler.cpp
+++ gui/src/GUI/ServerMessageHandler.cpp
@@ -5,11 +5,11 @@
 ** ServerMessageHandler
 */
 
+#include "ServerMessageHandler.hpp"
 #include <iostream>
 #include <sstream>
-
+#include "Data/Player.hpp"
 #include "Display.hpp"
-#include "ServerMessageHandler.hpp"
 
 namespace GUI {
 
@@ -18,6 +18,7 @@ ServerMessageHandler::ServerMessageHandler(bool debug, Display &display) : debug
     commandHandlers["msz"] = std::bind(&ServerMessageHandler::handleMapSize, this, std::placeholders::_1);
     commandHandlers["bct"] = std::bind(&ServerMessageHandler::handleTileContent, this, std::placeholders::_1);
     commandHandlers["tna"] = std::bind(&ServerMessageHandler::handleTeamNames, this, std::placeholders::_1);
+    commandHandlers["pnw"] = std::bind(&ServerMessageHandler::handleNewPlayer, this, std::placeholders::_1);
     commandHandlers["ppo"] = std::bind(&ServerMessageHandler::handlePlayerPosition, this, std::placeholders::_1);
     commandHandlers["plv"] = std::bind(&ServerMessageHandler::handlePlayerLevel, this, std::placeholders::_1);
     commandHandlers["pin"] = std::bind(&ServerMessageHandler::handlePlayerInventory, this, std::placeholders::_1);
@@ -30,7 +31,6 @@ ServerMessageHandler::ServerMessageHandler(bool debug, Display &display) : debug
     commandHandlers["pgt"] = std::bind(&ServerMessageHandler::handleResourceCollect, this, std::placeholders::_1);
     commandHandlers["pdi"] = std::bind(&ServerMessageHandler::handlePlayerDeath, this, std::placeholders::_1);
     commandHandlers["enw"] = std::bind(&ServerMessageHandler::handleEggLaid, this, std::placeholders::_1);
-    commandHandlers["eht"] = std::bind(&ServerMessageHandler::handleEggHatch, this, std::placeholders::_1);
     commandHandlers["ebo"] = std::bind(&ServerMessageHandler::handlePlayerConnectEgg, this, std::placeholders::_1);
     commandHandlers["edi"] = std::bind(&ServerMessageHandler::handleEggDeath, this, std::placeholders::_1);
     commandHandlers["sgt"] = std::bind(&ServerMessageHandler::handleTimeUnit, this, std::placeholders::_1);
@@ -66,7 +66,6 @@ void ServerMessageHandler::handleMapSize(const std::string &message)
         std::cout << "Map size: " << width << "x" << height << std::endl;
 }
 
-
 void ServerMessageHandler::handleTileContent(const std::string &message)
 {
     int x, y, q0, q1, q2, q3, q4, q5, q6;
@@ -79,20 +78,45 @@ void ServerMessageHandler::handleTileContent(const std::string &message)
     display.getMap().getTile(x, y).updateRessources(q0, q1, q2, q3, q4, q5, q6);
 }
 
-// TODO
 void ServerMessageHandler::handleTeamNames(const std::string &message)
 {
+    if (std::find(display.team.begin(), display.team.end(), message) == display.team.end())
+        display.team.push_back(message);
     if (debug)
         std::cout << "Team name: " << message << std::endl;
 }
 
-// TODO
+void ServerMessageHandler::handleNewPlayer(const std::string &message)
+{
+    int playerNumber, x, y, orientation, level;
+    std::string teamName;
+    std::istringstream iss(message);
+
+    iss >> playerNumber >> x >> y >> orientation >> level >> teamName;
+
+    auto &players = display.getMap().getPlayers();
+    players.emplace_back(x, y, playerNumber, teamName);
+
+    if (debug)
+        std::cout << "New player #" << playerNumber << " joined at (" << x << ", " << y << ") with orientation "
+                  << orientation << ", level " << level << ", and team " << teamName << "." << std::endl;
+}
+
 void ServerMessageHandler::handlePlayerPosition(const std::string &message)
 {
     int playerNumber, x, y, orientation;
     std::istringstream iss(message);
 
     iss >> playerNumber >> x >> y >> orientation;
+
+    auto &players = display.getMap().getPlayers();
+    for (auto &player : players) {
+        if (player.getId() == playerNumber) {
+            player.setPosition(x, y);
+            break;
+        }
+    }
+
     if (debug)
         std::cout << "Player #" << playerNumber << " position: (" << x << ", " << y << ") orientation: " << orientation
                   << std::endl;
@@ -104,6 +128,15 @@ void ServerMessageHandler::handlePlayerLevel(const std::string &message)
     std::istringstream iss(message);
 
     iss >> playerNumber >> level;
+
+    auto &players = display.getMap().getPlayers();
+    for (auto &player : players) {
+        if (player.getId() == playerNumber) {
+            player.setLevel(level);
+            break;
+        }
+    }
+
     if (debug)
         std::cout << "Player #" << playerNumber << " level: " << level << std::endl;
 }
@@ -114,12 +147,22 @@ void ServerMessageHandler::handlePlayerInventory(const std::string &message)
     std::istringstream iss(message);
 
     iss >> playerNumber >> x >> y >> q0 >> q1 >> q2 >> q3 >> q4 >> q5 >> q6;
+
+    auto &players = display.getMap().getPlayers();
+    for (auto &player : players) {
+        if (player.getId() == playerNumber) {
+            player.setPosition(x, y);
+            player.getInventory().update(q0, q1, q2, q3, q4, q5, q6);
+            break;
+        }
+    }
+
     if (debug)
         std::cout << "Player #" << playerNumber << " inventory: (" << x << ", " << y << ") " << q0 << " " << q1 << " "
                   << q2 << " " << q3 << " " << q4 << " " << q5 << " " << q6 << std::endl;
 }
 
-// TODO
+// TODO: handle expulsion
 void ServerMessageHandler::handleExpulsion(const std::string &message)
 {
     int playerNumber;
@@ -127,39 +170,58 @@ void ServerMessageHandler::handleExpulsion(const std::string &message)
 
     iss >> playerNumber;
     if (debug)
-        std::cout << "Player #" << playerNumber << " was expelled." << std::endl;
+        std::cout << "Player #" << playerNumber << " is expelled" << std::endl;
 }
 
-// TODO
+// TODO: handle broadcast
 void ServerMessageHandler::handleBroadcast(const std::string &message)
 {
+    int playerNumber;
+    std::string broadcastMessage;
+    std::istringstream iss(message);
+
+    iss >> playerNumber;
+    getline(iss, broadcastMessage);
+
     if (debug)
-        std::cout << "Broadcast message: " << message << std::endl;
+        std::cout << "Player #" << playerNumber << " broadcasted: " << broadcastMessage << std::endl;
 }
 
-// TODO
+// TODO: handle incantation start
 void ServerMessageHandler::handleIncantationStart(const std::string &message)
 {
-    int x, y, level;
     std::istringstream iss(message);
-
+    int x, y, level;
     iss >> x >> y >> level;
-    if (debug)
-        std::cout << "Incantation started at (" << x << ", " << y << ") for level " << level << std::endl;
+
+    std::vector<int> players;
+    int playerNumber;
+    while (iss >> playerNumber) {
+        players.push_back(playerNumber);
+    }
+
+    if (debug) {
+        std::cout << "Incantation started at (" << x << ", " << y << ") with level " << level << " by players: ";
+        for (auto p : players) {
+            std::cout << p << " ";
+        }
+        std::cout << std::endl;
+    }
 }
 
-// TODO
+// TODO: handle incantation end
 void ServerMessageHandler::handleIncantationEnd(const std::string &message)
 {
-    int x, y, result;
     std::istringstream iss(message);
-
+    int x, y, result;
     iss >> x >> y >> result;
+
     if (debug)
-        std::cout << "Incantation ended at (" << x << ", " << y << ") with result " << result << std::endl;
+        std::cout << "Incantation ended at (" << x << ", " << y << ") with result: " << (result ? "success" : "failure")
+                  << std::endl;
 }
 
-// TODO
+// TODO: handle egg laying
 void ServerMessageHandler::handleEggLaying(const std::string &message)
 {
     int playerNumber;
@@ -167,115 +229,147 @@ void ServerMessageHandler::handleEggLaying(const std::string &message)
 
     iss >> playerNumber;
     if (debug)
-        std::cout << "Player #" << playerNumber << " laid an egg." << std::endl;
+        std::cout << "Player #" << playerNumber << " has laid an egg" << std::endl;
 }
 
-// TODO
+// TODO: handle resource drop
 void ServerMessageHandler::handleResourceDrop(const std::string &message)
 {
-    int playerNumber, resourceNumber;
+    int playerNumber, resourceType;
     std::istringstream iss(message);
 
-    iss >> playerNumber >> resourceNumber;
+    iss >> playerNumber >> resourceType;
+
     if (debug)
-        std::cout << "Player #" << playerNumber << " dropped resource #" << resourceNumber << std::endl;
+        std::cout << "Player #" << playerNumber << " dropped resource type " << resourceType << std::endl;
 }
 
-// TODO
+// TODO: handle resource collect
 void ServerMessageHandler::handleResourceCollect(const std::string &message)
 {
-    int playerNumber, resourceNumber;
+    int playerNumber, resourceType;
     std::istringstream iss(message);
 
-    iss >> playerNumber >> resourceNumber;
+    iss >> playerNumber >> resourceType;
+
     if (debug)
-        std::cout << "Player #" << playerNumber << " collected resource #" << resourceNumber << std::endl;
+        std::cout << "Player #" << playerNumber << " collected resource type " << resourceType << std::endl;
 }
 
-// TODO
 void ServerMessageHandler::handlePlayerDeath(const std::string &message)
 {
     int playerNumber;
     std::istringstream iss(message);
 
     iss >> playerNumber;
+
+    auto &players = display.getMap().getPlayers();
+    players.erase(
+        std::remove_if(
+            players.begin(),
+            players.end(),
+            [playerNumber](const Data::Player &player) { return player.getId() == playerNumber; }
+        ),
+        players.end()
+    );
+
     if (debug)
-        std::cout << "Player #" << playerNumber << " has died." << std::endl;
+        std::cout << "Player #" << playerNumber << " has died" << std::endl;
 }
 
-// TODO
 void ServerMessageHandler::handleEggLaid(const std::string &message)
 {
     int eggNumber, playerNumber, x, y;
     std::istringstream iss(message);
 
     iss >> eggNumber >> playerNumber >> x >> y;
+
+    std::vector<Data::Egg> &eggs = display.getMap().getEggs();
+    eggs.emplace_back(Pos<int, 2>{x, y}, eggNumber);
+
     if (debug)
         std::cout << "Egg #" << eggNumber << " was laid by player #" << playerNumber << " at (" << x << ", " << y << ")"
                   << std::endl;
 }
 
-// TODO
-void ServerMessageHandler::handleEggHatch(const std::string &message)
-{
-    int eggNumber;
-    std::istringstream iss(message);
-
-    iss >> eggNumber;
-    if (debug)
-        std::cout << "Egg #" << eggNumber << " has hatched." << std::endl;
-}
-
-// TODO
 void ServerMessageHandler::handlePlayerConnectEgg(const std::string &message)
 {
     int eggNumber;
     std::istringstream iss(message);
 
     iss >> eggNumber;
-    if (debug)
-        std::cout << "Player connected to egg #" << eggNumber << std::endl;
+
+    auto &eggs = display.getMap().getEggs();
+    auto eggIter = std::remove_if(eggs.begin(), eggs.end(), [eggNumber](const Data::Egg &egg) {
+        return egg.getId() == eggNumber;
+    });
+    if (eggIter != eggs.end()) {
+        int x = eggIter->getPosition().x();
+        int y = eggIter->getPosition().y();
+
+        for (auto &player : display.getMap().getPlayers()) {
+            if (player.isHatched())
+                continue;
+            player.spawn();
+            player.setPosition(x, y);
+            break;
+        }
+        eggs.erase(eggIter, eggs.end());
+
+        if (debug)
+            std::cout << "Player connected to egg #" << eggNumber << " at (" << x << ", " << y << ")." << std::endl;
+    }
 }
 
-// TODO
 void ServerMessageHandler::handleEggDeath(const std::string &message)
 {
     int eggNumber;
     std::istringstream iss(message);
 
     iss >> eggNumber;
+
+    auto &eggs = display.getMap().getEggs();
+    eggs.erase(
+        std::remove_if(
+            eggs.begin(), eggs.end(), [eggNumber](const Data::Egg &egg) { return egg.getId() == eggNumber; }
+        ),
+        eggs.end()
+    );
+
     if (debug)
         std::cout << "Egg #" << eggNumber << " has died." << std::endl;
 }
 
 void ServerMessageHandler::handleTimeUnit(const std::string &message)
 {
-    int t;
+    int timeUnit;
     std::istringstream iss(message);
 
-    iss >> t;
-    display.updateTimeUnit(t);
+    iss >> timeUnit;
+    display.setTimeUnit(timeUnit);
     if (debug)
-        std::cout << "Time unit: " << t << std::endl;
+        std::cout << "Time unit set to " << timeUnit << std::endl;
 }
 
-// TODO
 void ServerMessageHandler::handleTimeUnitModification(const std::string &message)
 {
-    int t;
+    int timeUnit;
     std::istringstream iss(message);
 
-    iss >> t;
+    iss >> timeUnit;
+    display.setTimeUnit(timeUnit);
     if (debug)
-        std::cout << "Time unit modified to: " << t << std::endl;
+        std::cout << "Time unit modified to " << timeUnit << std::endl;
 }
 
+// TODO: handle end game
 void ServerMessageHandler::handleEndGame(const std::string &message)
 {
     if (debug)
-        std::cout << "End of game. Winning team: " << message << std::endl;
+        std::cout << "Game over. Message: " << message << std::endl;
 }
 
+// TODO: handle message from server
 void ServerMessageHandler::handleMessageFromServer(const std::string &message)
 {
     if (debug)
@@ -285,13 +379,13 @@ void ServerMessageHandler::handleMessageFromServer(const std::string &message)
 void ServerMessageHandler::handleUnknownCommand(const std::string &message)
 {
     if (debug)
-        std::cout << "Unknown command received from server: " << message << std::endl;
+        std::cout << "Unknown command: " << message << std::endl;
 }
 
 void ServerMessageHandler::handleCommandParameter(const std::string &message)
 {
     if (debug)
-        std::cout << "Command parameter issue: " << message << std::endl;
+        std::cout << "Bad parameter for command: " << message << std::endl;
 }
 
-}; // namespace GUI
+} // namespace GUI
