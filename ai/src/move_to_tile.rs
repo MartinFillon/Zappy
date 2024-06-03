fb2a80ff0d19ff649cb9d60e17fadf53a22ea376
Alexandre VIGOUREUX <alexandre.vigoureux@epitech.eu>
2024-06-03 18:40:56 +0200

    fix(use): now importing Direction and turn() functions directly, as well as move_up()

--- ai/src/move_to_tile.rs
+++ ai/src/move_to_tile.rs
@@ -7,7 +7,11 @@
 
 use log::info;
 
-use crate::{commands::move_up, commands::turn, tcp::TcpClient};
+use crate::{
+    commands::move_up::move_up,
+    commands::turn::{turn, Direction},
+    tcp::TcpClient,
+};
 
 fn get_row(tile: usize, lvl: usize) -> Option<i32> {
     let mut tile_count: usize = 0;
@@ -56,30 +60,30 @@ fn get_tile_coordinates(tile: usize, lvl: usize) -> Option<(i32, i32)> {
 }
 
 async fn move_left(client: &mut TcpClient, x: i32) -> bool {
-    if !turn::turn(client, turn::Direction::Left).await {
+    if !turn(client, Direction::Left).await {
         return false;
     }
     for _ in 0..=(-x) {
-        if !move_up::move_up(client).await {
+        if !move_up(client).await {
             return false;
         }
     }
-    if !turn::turn(client, turn::Direction::Right).await {
+    if !turn(client, Direction::Right).await {
         return false;
     }
     true
 }
 
 async fn move_right(client: &mut TcpClient, x: i32) -> bool {
-    if !turn::turn(client, turn::Direction::Right).await {
+    if !turn(client, Direction::Right).await {
         return false;
     }
     for _ in 0..=x {
-        if !move_up::move_up(client).await {
+        if !move_up(client).await {
             return false;
         }
     }
-    if !turn::turn(client, turn::Direction::Left).await {
+    if !turn(client, Direction::Left).await {
         return false;
     }
     true
@@ -93,7 +97,7 @@ pub async fn move_to_tile(client: &mut TcpClient, tile: usize, lvl: usize) -> bo
                 return false;
             }
             for _ in 0..=y {
-                if !move_up::move_up(client).await {
+                if !move_up(client).await {
                     return false;
                 }
             }
