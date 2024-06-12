8d6036e3da903a91c1e5b0005a87f8e8ed0cefb4
MartinFillon <martin.fillon@epitech.eu>
2024-06-12 22:33:51 +0200

    docs: add documentation for gui commands

--- server/include/core/gui/defs.h
+++ server/include/core/gui/defs.h
@@ -10,12 +10,65 @@
 #include "core/router/route.h"
 #include "core/types/client.h"
 
+/**
+ * @brief Retrieve the size of the map.
+ * @param cli Clients' informations/Ai that drop an object.
+ * @param s state of the program.
+ */
 void map_size(client_t *c, command_state_t *s);
+
+/**
+ * @brief Retrieve the content of a tile.
+ * @param cli Clients' informations/Ai that drop an object.
+ * @param s state of the program.
+ */
 void map_content_tile(client_t *c, command_state_t *s);
+
+/**
+ * @brief Retrieve the content of the whole map.
+ * @param cli Clients' informations/Ai that drop an object.
+ * @param s state of the program.
+ */
 void map_content_full(client_t *c, command_state_t *s);
+
+/**
+ * @brief Retrieve the team names.
+ * @param cli Clients' informations/Ai that drop an object.
+ * @param s state of the program.
+ */
 void team_names(client_t *c, command_state_t *s);
+
+/**
+ * @brief Retrieve the player position.
+ * @param cli Clients' informations/Ai that drop an object.
+ * @param s state of the program.
+ */
 void player_position(client_t *c, command_state_t *s);
+
+/**
+ * @brief Retrieve the player level.
+ * @param cli Clients' informations/Ai that drop an object.
+ * @param s state of the program.
+ */
 void player_level(client_t *c, command_state_t *s);
+
+/**
+ * @brief Retrieve the player inventory.
+ * @param cli Clients' informations/Ai that drop an object.
+ * @param s state of the program.
+ */
 void player_inventory(client_t *c, command_state_t *s);
+
+/**
+ * @brief update the frequency of the server.
+ * @param cli Clients' informations/Ai that drop an object.
+ * @param s state of the program.
+ */
 void update_time(client_t *c, command_state_t *s);
+
+/**
+ * @brief Retrieve the frequency of the server.
+ * @param cli Clients' informations/Ai that drop an object.
+ * @param s state of the program.
+ */
 void request_time(client_t *c, command_state_t *s);
