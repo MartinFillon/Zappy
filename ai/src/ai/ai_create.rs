56cd7a4eda112d8c6923c465e452510e8ee719a6
rahul.chander <rahul.chander@epitech.eu>
2024-06-12 15:13:25 +0200

    feat(ai.rs): Handling Core for ais

---
+++ ai/src/ai/ai_create.rs
@@ -0,0 +1,28 @@
+//
+// EPITECH PROJECT, 2024
+// Zappy
+// File description:
+// ai_create
+//
+
+use super::AI;
+
+pub fn start_empress_ai(ai: AI)
+{
+    println!("Init and update AI here: {ai}.");
+}
+
+pub fn start_queen_ai(ai: AI)
+{
+    println!("Init and update AI here: {ai}.");
+}
+
+pub fn start_fetus_ai(ai: AI)
+{
+    println!("Init and update AI here: {ai}.");
+}
+
+pub fn start_knight_ai(ai: AI)
+{
+    println!("Init and update AI here: {ai}.");
+}
