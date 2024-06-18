f7bd040803eaf584071c1fbd299184c8443abc15
MartinFillon <martin.fillon@hotmail.com>
2024-06-18 15:21:40 +0200

    fix: time request was sending garbage

--- server/core/src/client/send.c
+++ server/core/src/client/send.c
@@ -8,12 +8,18 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "core/types/client.h"
+#include "logger.h"
 
 void send_client(client_t *client, char *fmt, ...)
 {
     va_list args;
+    va_list cpy;
 
     va_start(args, fmt);
+    va_copy(cpy, args);
+    logs(DEBUG, "Client %d result buffer: ", client->fd);
+    vdprintf(2, fmt, args);
     vdprintf(client->fd, fmt, args);
     va_end(args);
+    va_end(cpy);
 }
