66b1b1600b92ab972a1b4e74d87bcfbbf99b3344
MartinFillon <martin.fillon@hotmail.com>
2024-06-18 13:18:02 +0200

    fix!: buffer was overflowing at ulong max

--- server/core/src/server/buffer.c
+++ server/core/src/server/buffer.c
@@ -25,9 +25,13 @@ int handle_buffer(client_t *c)
     struct vector_str_t *lines = str_split(c->io.req, "\n");
 
     logs(DEBUG, "Client %d has %d lines in buffer\n", c->fd, lines->size);
+    if (lines->size == 0) {
+        vec_destroy_vector_str_t(lines);
+        return 0;
+    }
     if (lines->size == 1)
         return one_line_case(c, lines);
-    for (size_t i = 0; i < lines->size - 1; i++)
+    for (int i = 0; i < (int)lines->size - 1; i++)
         queue_pushfront_queue_command_t(c->commands, lines->data[i]);
     str_free(c->io.req);
     c->io.req = lines->data[lines->size - 1];
--- server/lib/str/add.c
+++ server/lib/str/add.c
@@ -5,13 +5,18 @@
 ** add
 */
 
+#include <stdint.h>
 #include <string.h>
+#include "logger.h"
 #include "str.h"
 
 void str_scadd(str_t *this, char const *s)
 {
-    size_t len = strlen(s);
-
+    unsigned long long len = strlen(s);
+    logs(ERROR_LEVEL, "%p\n", this);
+    if ((unsigned long long)this->size + len > SIZE_MAX) {
+        return str_clear(this);
+    }
     if (this->size + len >= this->capacity) {
         str_resize(this);
         return str_scadd(this, s);
