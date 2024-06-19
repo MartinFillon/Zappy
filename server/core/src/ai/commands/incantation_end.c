45c0b7a31b73dd8512dc18354f031edefcb48b27
MartinFillon <martin.fillon@hotmail.com>
2024-06-19 09:50:41 +0200

    fix: change events for incantation end

--- server/core/src/ai/commands/incantation_end.c
+++ server/core/src/ai/commands/incantation_end.c
@@ -8,26 +8,33 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "core/client.h"
+#include "core/map.h"
 #include "core/router/route.h"
 #include "core/types/ai.h"
 #include "core/types/client.h"
 #include "core/types/game.h"
 #include "core/types/incantation.h"
-#include "core/map.h"
 #include "core/types/object.h"
 #include "core/types/position.h"
 #include "logger.h"
 #include "macros.h"
 #include "utils.h"
 
-static void increment_all_levels(game_t *game, int id_incant)
+static void increment_all_levels(
+    game_t *game,
+    int id_incant,
+    struct client_list *cls
+)
 {
     struct vector_ai_t *ais = game->ais;
 
     for (size_t i = 0; i < ais->size; i++) {
-        if (ais->data[i]->incant.is_incant
-            && ais->data[i]->incant.id_incant == id_incant) {
+        if (ais->data[i]->incant.is_incant &&
+            ais->data[i]->incant.id_incant == id_incant) {
             ais->data[i]->level++;
+            broadcast_to(
+                GUI, cls, "plv %d %ld\n", ais->data[i]->id, ais->data[i]->level
+            );
         }
     }
 }
@@ -46,11 +53,7 @@ static void consume_tile_incantation(
         tile->content[i] -= tile_req[i];
 }
 
-static size_t count_nb_ai_incant(
-    game_t *g,
-    struct tile_s *tile,
-    ai_t *ai
-)
+static size_t count_nb_ai_incant(game_t *g, struct tile_s *tile, ai_t *ai)
 {
     struct vector_ai_t *ais = g->ais;
     ai_t *oth = NULL;
@@ -71,16 +74,16 @@ static bool verif_end_specification(ai_t *ai, game_t *game)
 {
     pos_t *pos = &ai->pos;
     size_t idx = ai->level - 1;
-    size_t nb_player = count_nb_ai_incant(
-        game, &game->map->arena[pos->y][pos->x], ai);
+    size_t nb_player =
+        count_nb_ai_incant(game, &game->map->arena[pos->y][pos->x], ai);
 
     if (nb_player < incant_req[idx].nb_player) {
         logs(DEBUG, "Not enough player for end_incantation.\n");
         return false;
     }
     if (!verif_tile_requirement(
-        game->map->arena[pos->y][pos->x].content,
-        incant_req[idx].ressource)) {
+            game->map->arena[pos->y][pos->x].content, incant_req[idx].ressource
+        )) {
         logs(DEBUG, "Tiles requirements are not fullfilled in end.\n");
         return false;
     }
@@ -111,15 +114,14 @@ static void end_ais_elevation(
     prepare_response_cat(&cli->io, msg);
 }
 
-static void send_incantation_end(client_t *cli, struct client_list *clients)
+static void send_incantation_end(
+    client_t *cli,
+    struct client_list *clients,
+    int result
+)
 {
     broadcast_to(
-        GUI,
-        clients,
-        "pie %d %d %d\n",
-        cli->ai->pos.x,
-        cli->ai->pos.y,
-        cli->ai->level
+        GUI, clients, "pie %d %d %d\n", cli->ai->pos.x, cli->ai->pos.y, result
     );
 }
 
@@ -128,21 +130,22 @@ void handle_end_incantation(client_t *cli, command_state_t *s)
     bool first = cli->ai->incant.last_verif;
     char msg[BUFF_SIZE] = {0};
 
-    cli->ai->incant.last_verif =
-        verif_end_specification(cli->ai, s->game);
+    cli->ai->incant.last_verif = verif_end_specification(cli->ai, s->game);
     if (first == false) {
         unfreeze_ai(cli->ai);
         logs(DEBUG, "Start incantation failed, so end too.\n");
+        send_incantation_end(cli, s->clients, 0);
         return prepare_response_cat(&cli->io, "ko\n");
     }
     if (cli->ai->incant.last_verif) {
         consume_tile_incantation(
-            cli->ai->level - 1, s->game->map, cli->ai->pos.y, cli->ai->pos.x);
-        increment_all_levels(s->game, cli->ai->id);
+            cli->ai->level - 1, s->game->map, cli->ai->pos.y, cli->ai->pos.x
+        );
+        increment_all_levels(s->game, cli->ai->id, s->clients);
         sprintf(msg, "Current level: %ld\n", cli->ai->level);
     } else {
         sprintf(msg, "ko\n");
     }
     end_ais_elevation(s->clients, cli, msg);
-    send_incantation_end(cli, s->clients);
+    send_incantation_end(cli, s->clients, 1);
 }
