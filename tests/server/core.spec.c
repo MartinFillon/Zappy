6849e2ef3cc460b3f5fd79728dac881540e9b70e
MartinFillon <martin.fillon@epitech.eu>
2024-06-20 22:36:25 +0200

    test: finish testing gui commands

--- tests/server/core.spec.c
+++ tests/server/core.spec.c
@@ -155,3 +155,161 @@ Test(core, test_player_position)
     player_position(cli, &com);
     cr_assert_str_eq(cli->io.res->data, "ppo 0 5 1 2 1 Team1\n");
 }
+
+Test(core, test_player_position_invalid_args)
+{
+    zappy_t z;
+
+    init_server(&z);
+    client_t *cli = new_fake_gui_client(z.clients);
+    struct vector_str_t *args = vec_create_vector_str_t(1);
+    vec_pushback_vector_str_t(args, str_snew("ppo"));
+    vec_pushback_vector_str_t(args, str_snew("100"));
+    command_state_t com = {args, z.clients, &z.game};
+    str_clear(cli->io.res);
+
+    player_position(cli, &com);
+    cr_assert_str_eq(cli->io.res->data, "sbp\n");
+}
+
+Test(core, test_player_position_invalid_args2)
+{
+    zappy_t z;
+
+    init_server(&z);
+    client_t *cli = new_fake_gui_client(z.clients);
+    struct vector_str_t *args = vec_create_vector_str_t(1);
+    vec_pushback_vector_str_t(args, str_snew("ppo"));
+    vec_pushback_vector_str_t(args, str_snew("a"));
+    command_state_t com = {args, z.clients, &z.game};
+
+    player_position(cli, &com);
+    cr_assert_str_eq(cli->io.res->data, "sbp\n");
+}
+
+Test(core, test_player_level)
+{
+    zappy_t z;
+
+    init_server(&z);
+    client_t *cli = new_fake_gui_client(z.clients);
+    client_t *ai =
+        new_fake_ai_client(&z.game.teams->data[0], &z.game, z.clients);
+    struct vector_str_t *args = vec_create_vector_str_t(1);
+    vec_pushback_vector_str_t(args, str_snew("plv"));
+    vec_pushback_vector_str_t(args, str_snew("0"));
+    command_state_t com = {args, z.clients, &z.game};
+
+    str_clear(cli->io.res);
+    player_level(cli, &com);
+    cr_assert_str_eq(cli->io.res->data, "plv 0 1");
+}
+
+Test(core, test_player_level_invalid_args)
+{
+    zappy_t z;
+
+    init_server(&z);
+    client_t *cli = new_fake_gui_client(z.clients);
+    struct vector_str_t *args = vec_create_vector_str_t(1);
+    vec_pushback_vector_str_t(args, str_snew("plv"));
+    vec_pushback_vector_str_t(args, str_snew("100"));
+    command_state_t com = {args, z.clients, &z.game};
+    str_clear(cli->io.res);
+    player_level(cli, &com);
+    cr_assert_str_eq(cli->io.res->data, "sbp\n");
+}
+
+Test(core, test_player_level_invalid_args2)
+{
+    zappy_t z;
+
+    init_server(&z);
+    client_t *cli = new_fake_gui_client(z.clients);
+    struct vector_str_t *args = vec_create_vector_str_t(1);
+    vec_pushback_vector_str_t(args, str_snew("plv"));
+    vec_pushback_vector_str_t(args, str_snew("a"));
+    command_state_t com = {args, z.clients, &z.game};
+
+    player_level(cli, &com);
+    cr_assert_str_eq(cli->io.res->data, "sbp\n");
+}
+
+Test(core, test_player_inventory)
+{
+    zappy_t z;
+
+    init_server(&z);
+    client_t *cli = new_fake_gui_client(z.clients);
+    client_t *ai =
+        new_fake_ai_client(&z.game.teams->data[0], &z.game, z.clients);
+    struct vector_str_t *args = vec_create_vector_str_t(1);
+    vec_pushback_vector_str_t(args, str_snew("pin"));
+    vec_pushback_vector_str_t(args, str_snew("0"));
+    command_state_t com = {args, z.clients, &z.game};
+
+    str_clear(cli->io.res);
+    player_inventory(cli, &com);
+    cr_assert_str_eq(cli->io.res->data, "pin 0 5 1 10 0 0 0 0 0 0\n");
+}
+
+Test(core, test_player_inventory_invalid_args)
+{
+    zappy_t z;
+
+    init_server(&z);
+    client_t *cli = new_fake_gui_client(z.clients);
+    struct vector_str_t *args = vec_create_vector_str_t(1);
+    vec_pushback_vector_str_t(args, str_snew("pin"));
+    vec_pushback_vector_str_t(args, str_snew("100"));
+    command_state_t com = {args, z.clients, &z.game};
+    str_clear(cli->io.res);
+    player_inventory(cli, &com);
+    cr_assert_str_eq(cli->io.res->data, "sbp\n");
+}
+
+Test(core, test_player_inventory_invalid_args2)
+{
+    zappy_t z;
+
+    init_server(&z);
+    client_t *cli = new_fake_gui_client(z.clients);
+    struct vector_str_t *args = vec_create_vector_str_t(1);
+    vec_pushback_vector_str_t(args, str_snew("pin"));
+    vec_pushback_vector_str_t(args, str_snew("a"));
+    command_state_t com = {args, z.clients, &z.game};
+
+    player_inventory(cli, &com);
+    cr_assert_str_eq(cli->io.res->data, "sbp\n");
+}
+
+Test(core, test_map_content_full)
+{
+    zappy_t z;
+
+    init_server(&z);
+    client_t *cli = new_fake_gui_client(z.clients);
+    struct vector_str_t *args = vec_create_vector_str_t(1);
+    vec_pushback_vector_str_t(args, str_snew("mct"));
+    command_state_t com = {args, z.clients, &z.game};
+
+    map_content_full(cli, &com);
+    struct vector_str_t *res = str_split(cli->io.res, "\n");
+    cr_assert_eq(res->size, z.game.map->x * z.game.map->y);
+}
+
+Test(core, test_tile_content_invalid)
+{
+    zappy_t z;
+
+    init_server(&z);
+    client_t *cli = new_fake_gui_client(z.clients);
+    struct vector_str_t *args = vec_create_vector_str_t(1);
+    vec_pushback_vector_str_t(args, str_snew("bct"));
+    vec_pushback_vector_str_t(args, str_snew("a"));
+    vec_pushback_vector_str_t(args, str_snew("b100"));
+    command_state_t com = {args, z.clients, &z.game};
+
+    map_content_tile(cli, &com);
+    cr_assert_str_eq(cli->io.res->data, "sbp\n");
+}
