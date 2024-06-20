0c76fc8a206dba3699898ea7241fa7c6771656d6
MartinFillon <martin.fillon@hotmail.com>
2024-06-20 17:26:38 +0200

    test: finish loader spec

--- tests/server/loader.spec.c
+++ tests/server/loader.spec.c
@@ -19,3 +19,23 @@ Test(loader, success_case)
     cr_assert_not_null(lib.destroy);
     close_dhl(&lib);
 }
+
+Test(loader, fail_case)
+{
+    lib_t lib = open_dhl("lou.so");
+
+    cr_assert_null(lib.handle);
+    cr_assert_null(lib.loop);
+    cr_assert_null(lib.init);
+    cr_assert_null(lib.destroy);
+}
+
+Test(loader, missing_function)
+{
+    lib_t lib = open_dhl("../tests/server/missing_function.so");
+
+    cr_assert_null(lib.handle);
+    cr_assert_null(lib.loop);
+    cr_assert_null(lib.init);
+    cr_assert_null(lib.destroy);
+}
