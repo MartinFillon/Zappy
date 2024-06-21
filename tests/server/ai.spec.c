8f553d830b6810e13d4d507b579968c0a41ed74f
MartinFillon <martin.fillon@hotmail.com>
2024-06-21 16:15:19 +0200

    test: add more ai testing

--- tests/server/ai.spec.c
+++ tests/server/ai.spec.c
@@ -7,10 +7,11 @@
 
 #include <stdio.h>
 #include <criterion/criterion.h>
+#include "core/types/object.h"
 
 #include "fake_server.h"
 
-Test(core, unknown_ai_command)
+Test(core_ai, unknown_ai_command)
 {
     zappy_t z;
 
@@ -22,7 +23,7 @@ Test(core, unknown_ai_command)
     cr_assert_str_eq(cli->io.res->data, "ko\n");
 }
 
-Test(core, move_by_dir_up)
+Test(core_ai, move_by_dir_up)
 {
     zappy_t z;
 
@@ -35,7 +36,7 @@ Test(core, move_by_dir_up)
     cr_assert_eq(pos.y, 4);
 }
 
-Test(core, move_by_dir_down)
+Test(core_ai, move_by_dir_down)
 {
     zappy_t z;
 
@@ -48,7 +49,7 @@ Test(core, move_by_dir_down)
     cr_assert_eq(pos.y, 6);
 }
 
-Test(core, move_by_dir_left)
+Test(core_ai, move_by_dir_left)
 {
     zappy_t z;
 
@@ -61,7 +62,7 @@ Test(core, move_by_dir_left)
     cr_assert_eq(pos.y, 5);
 }
 
-Test(core, move_by_dir_right)
+Test(core_ai, move_by_dir_right)
 {
     zappy_t z;
 
@@ -74,7 +75,7 @@ Test(core, move_by_dir_right)
     cr_assert_eq(pos.y, 5);
 }
 
-Test(core, move_ai)
+Test(core_ai, move_ai)
 {
     zappy_t z;
 
@@ -87,7 +88,7 @@ Test(core, move_ai)
     cr_assert_eq(cli->ai->pos.y, 0);
 }
 
-Test(core, handle_forward_with_gui)
+Test(core_ai, handle_forward_with_gui)
 {
     zappy_t z;
 
@@ -116,7 +117,7 @@ Test(core, handle_forward_with_gui)
     cr_assert_str_eq(cli->io.res->data, event);
 }
 
-Test(core, rotate_left)
+Test(core_ai, rotate_left)
 {
     zappy_t z;
 
@@ -145,7 +146,7 @@ Test(core, rotate_left)
     cr_assert_str_eq(cli->io.res->data, event);
 }
 
-Test(core, rotate_right)
+Test(core_ai, rotate_right)
 {
     zappy_t z;
 
@@ -173,3 +174,81 @@ Test(core, rotate_right)
     cr_assert_str_eq(ai->io.res->data, "ok\n");
     cr_assert_str_eq(cli->io.res->data, event);
 }
+
+Test(core_ai, connect_nbr)
+{
+    zappy_t z;
+
+    init_server(&z);
+    client_t *ai =
+        new_fake_ai_client(&z.game.teams->data[0], &z.game, z.clients);
+
+    struct vector_str_t *args = vec_create_vector_str_t(1);
+    vec_pushback_vector_str_t(args, str_snew("Right"));
+    command_state_t com = {args, z.clients, &z.game};
+
+    char *res = NULL;
+    handle_connect_nbr(ai, &com);
+    asprintf(&res, "%ld\n", ai->ai->team->eggs->size);
+    cr_assert_str_eq(ai->io.res->data, res);
+}
+
+Test(core_ai, inventory)
+{
+    zappy_t z;
+
+    init_server(&z);
+    client_t *cli =
+        new_fake_ai_client(&z.game.teams->data[0], &z.game, z.clients);
+
+    struct vector_str_t *args = vec_create_vector_str_t(1);
+    vec_pushback_vector_str_t(args, str_snew("quoi"));
+    command_state_t com = {args, z.clients, &z.game};
+
+    char *res = NULL;
+    handle_inventory(cli, &com);
+    asprintf(
+        &res,
+        "[%s %ld, %s %ld, %s %ld, %s %ld, %s %ld, %s %ld, %s %ld]\n",
+        "food",
+        cli->ai->inventory[FOOD],
+        "linemate",
+        cli->ai->inventory[LINEMATE],
+        "deraumere",
+        cli->ai->inventory[DERAUMERE],
+        "sibur",
+        cli->ai->inventory[SIBUR],
+        "mendiane",
+        cli->ai->inventory[MENDIANE],
+        "phiras",
+        cli->ai->inventory[PHIRAS],
+        "thystame",
+        cli->ai->inventory[THYSTAME]
+    );
+    cr_assert_str_eq(cli->io.res->data, res);
+}
+
+Test(core_ai, take_object)
+{
+    zappy_t z;
+
+    init_server(&z);
+    client_t *cli =
+        new_fake_ai_client(&z.game.teams->data[0], &z.game, z.clients);
+
+    struct vector_str_t *args = vec_create_vector_str_t(1);
+    vec_pushback_vector_str_t(args, str_snew("Take"));
+    vec_pushback_vector_str_t(args, str_snew("thystame"));
+    command_state_t com = {args, z.clients, &z.game};
+
+    z.game.map->arena[cli->ai->pos.y][cli->ai->pos.x].content[THYSTAME] = 1;
+
+    handle_take_object(cli, &com);
+    cr_assert_eq(cli->ai->inventory[THYSTAME], 1);
+    cr_assert_eq(
+        z.game.map->arena[cli->ai->pos.y][cli->ai->pos.x].content[THYSTAME], 0
+    );
+    cr_assert_str_eq(cli->io.res->data, "ok\n");
+}
+
+
